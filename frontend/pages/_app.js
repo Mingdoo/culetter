601f2f3d75a391770fe3e4338d115ec21b7395b8
김정연 <dkfkqldk97@naver.com>
2022-03-21 07:14:45 +0000

    Revert "Merge branch 'front/feat-login' into 'master'"
    This reverts merge request !10

--- frontend/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import "../styles/globals.css";
-import "../styles/Landing.module.css";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
