60a1d8ff140c6b1b50c0fce41317b8391680e552
김도현 <kdh494849@gmail.com>
2022-03-23 12:06:54 +0900

    feat: 임시 비밀번호 발급 API

--- backend/src/main/java/com/culetter/api/controller/AuthEmailController.java
+++ backend/src/main/java/com/culetter/api/controller/AuthEmailController.java
@@ -1,17 +1,14 @@
 package com.culetter.api.controller;
 
 import com.culetter.api.dto.AuthEmailDto;
+import com.culetter.api.dto.MemberDto;
 import com.culetter.api.service.AuthEmailService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Map;
 
 @Slf4j
 @RestController
@@ -36,5 +33,11 @@ public class AuthEmailController {
             return ResponseEntity.status(HttpStatus.OK).body("이메일 인증이 완료되었습니다.");
         }
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("이메일 인증이 실패하였습니다. 인증 코드를 확인해주세요.");
+    }
+
+    @PostMapping("/pwreset")
+    public ResponseEntity<String> requestTempPassword(@Valid @RequestBody MemberDto.PasswordResetRequest passwordResetRequest) {
+        authEmailService.sendTempPassword(passwordResetRequest.getEmail());
+        return ResponseEntity.status(HttpStatus.OK).body("임시 비밀번호가 이메일로 전송되었습니다.");
     }
 }
--- backend/src/main/java/com/culetter/api/service/AuthEmailService.java
+++ backend/src/main/java/com/culetter/api/service/AuthEmailService.java
@@ -3,4 +3,5 @@ package com.culetter.api.service;
 public interface AuthEmailService {
     void sendAuthEmail(String email);
     boolean checkCode(String email, String code);
+    void sendTempPassword(String email);
 }
