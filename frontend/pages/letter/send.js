0e4c03bb2e4c05032928f89fd499e73792e2e384
KES <mapleapril@naver.com>
2022-03-29 21:36:22 +0900

    feat:홈으로, 보낸 편지

--- frontend/pages/letter/send.js
+++ frontend/pages/letter/send.js
@@ -1,33 +1,26 @@
 import { Box, Typography, Button, TextField, Grid } from "@mui/material";
-import Slider from "@mui/material/Slider";
-import { useEffect, useState } from "react";
-import { styled, alpha } from "@mui/material/styles";
+import { useEffect, useState, useContext } from "react";
 import ContentCopyRoundedIcon from "@mui/icons-material/ContentCopyRounded";
-
-import Letter from "../../components/mail/sent/Letter";
+import ContentsContext from "../../contexts/ContentsContext";
+import ShareIcon from "@mui/icons-material/Share";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from "react-toastify";
 import Test from "../../components/letter/send/test";
-
-const SuccessSlider = styled(Button)(({ theme }) => ({
-  color: "#00000080",
-  backgroundColor: "#FEE500",
-  "& .MuiSlider-thumb": {
-    "&:hover, &.Mui-focusVisible": {
-      boxShadow: `0px 0px 0px 8px ${alpha(theme.palette.success.main, 0.16)}`,
-    },
-    "&.Mui-active": {
-      boxShadow: `0px 0px 0px 14px ${alpha(theme.palette.success.main, 0.16)}`,
-    },
-  },
-}));
+import Header from "../../components/Header";
+import Router from "next/router";
 
 export default function Send() {
+  // memberId가 있으면 카카오톡으로 알리기 아니면 링크 공유
+  // const { memberId, setMemberId } = useContext(ContentsContext);
+  const memberId = "temp";
   const Explain = useEffect(() => {
     // 내 js 키 숨기기.
     console.log(window.location.href);
-    Kakao.init("3a6df59af8e42ef7045cfd0b2303169c");
+    // Kakao.init("3a6df59af8e42ef7045cfd0b2303169c");
   }, []);
+
+  const toHome = () => Router.push("/");
+  const toMailSent = () => Router.push("/mail/sent");
 
   const shareKakao = () => {
     Kakao.Link.sendDefault({
@@ -99,24 +92,14 @@ export default function Send() {
     <Box
       component="div"
       sx={{
+        position: "relative",
         width: 420,
         minHeight: "100vh",
         mx: "auto",
         bgcolor: "#FCFAEF",
       }}
     >
-      <Box
-        component="span"
-        sx={{
-          fontSize: 24,
-          display: "flex",
-          justifyContent: "center",
-          pt: "1rem",
-          fontFamily: "Gowun Dodum",
-        }}
-      >
-        전송
-      </Box>
+      <Header title="전송"></Header>
       <Typography
         sx={{
           display: "flex",
@@ -137,47 +120,104 @@ export default function Send() {
         공유를 통해 다른 사람에게 편지를 전달해주세요!
       </Typography>
       {/* 가운데 위치시키는 방법은?? */}
+
       <Box component="div" sx={{ mt: "7rem" }}>
         {/* 편지 봉투 디자인 좀 바꾸자.... */}
-        {/* letter 쓸거면 click 막아야 함 */}
-        <Letter width={300} date=""></Letter>
-        <Box
-          sx={{ width: 1, display: "flex", justifyContent: "center", mt: 1 }}
+
+        {/* 카톡 공유 */}
+        {memberId ? (
+          <Box
+            sx={{ width: 1, display: "flex", justifyContent: "center", mt: 1 }}
+          >
+            <Button
+              sx={{ backgroundColor: "#FEE500", color: "black" }}
+              onClick={shareKakao}
+              startIcon={<Test />}
+            >
+              카카오톡으로 알리기
+            </Button>
+          </Box>
+        ) : (
+          <Box>
+            <Grid container sx={{ px: 2 }}>
+              <Grid item xs={9}>
+                <TextField
+                  disabled
+                  id="linkShare"
+                  defaultValue={test}
+                  variant="standard"
+                  sx={{ color: "black", width: "100%" }}
+                />
+              </Grid>
+              <Grid item xs={1}>
+                <Button
+                  sx={{ color: "black", fontFamily: "Gowun Batang" }}
+                  onClick={copyData}
+                >
+                  복사
+                </Button>
+              </Grid>
+              <Grid item xs={1}>
+                <Button
+                  sx={{ color: "black", fontFamily: "Gowun Batang", pl: 3 }}
+                  onClick={shareMobile}
+                >
+                  공유
+                </Button>
+              </Grid>
+            </Grid>
+            <ToastContainer
+              toastStyle={{ backgroundColor: "#12121290", color: "white" }}
+            />
+          </Box>
+        )}
+      </Box>
+      <Grid
+        container
+        sx={{
+          position: "absolute",
+          bottom: 35,
+          width: 1,
+        }}
+        component="div"
+      >
+        <Grid item xs={2}></Grid>
+        <Grid
+          item
+          xs={3}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
         >
           <Button
-            sx={{ backgroundColor: "#FEE500", color: "black" }}
-            onClick={shareKakao}
-            startIcon={<Test />}
+            sx={{
+              fontFamily: "Gowun Batang",
+              fontSize: 18,
+              color: "black",
+            }}
+            onClick={toMailSent}
           >
-            카카오톡으로 알리기
+            보낸 편지
           </Button>
-        </Box>
-        <Grid container>
-          <Grid item xs={8}>
-            <TextField
-              disabled
-              id="standard-disabled"
-              // label="Disabled"
-              defaultValue={test}
-              variant="outlined"
-              sx={{ color: "black", width: "100%" }}
-            />
-          </Grid>
-          <Grid item xs={2}>
-            <Button
-              startIcon={<ContentCopyRoundedIcon />}
-              onClick={copyData}
-            ></Button>
-          </Grid>
-          <Grid item xs={2}>
-            <Button onClick={shareMobile}>공유</Button>
-          </Grid>
         </Grid>
-        <ToastContainer
-          toastStyle={{ backgroundColor: "#12121290", color: "white" }}
-        />
-        {/* <SuccessSlider onClick={shareMobile}>공유</SuccessSlider> */}
-      </Box>
+        <Grid xs={2}></Grid>
+        <Grid
+          item
+          xs={3}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Button
+            sx={{ fontFamily: "Gowun Batang", fontSize: 18, color: "black" }}
+            onClick={toHome}
+          >
+            홈으로
+          </Button>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
