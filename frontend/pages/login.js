fcc131b91a90b72dfcc71b7d3dbf1421cd2b66ab
dkfkqldk97 <dkfkqldk97@naver.com>
2022-03-31 16:43:47 +0900

    chore : fix register layout

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -3,9 +3,14 @@ import LoginForm from "../components/login/LoginForm";
 import LoginBtn from "../components/login/LoginBtn";
 import Postbox from "../components/postbox/Postbox";
 import { makeStyles } from "@material-ui/core/styles";
+import { Box, Typography } from "@mui/material";
 
 const Login = () => {
-  return <Postbox title="LOGIN"></Postbox>;
+  return (
+    <Box sx={{ width: 420, mx: "auto" }}>
+      <Postbox title="LOGIN"></Postbox>;
+    </Box>
+  );
 };
 
 export default Login;
