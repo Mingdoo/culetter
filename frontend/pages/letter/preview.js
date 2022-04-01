f1b280c3e4ffc08cf8d1a8f3bb223035843b42e0
김은송 <mapleapril@naver.com>
2022-04-01 08:51:20 +0000

    fix:fix conflict

--- frontend/pages/letter/preview.js
+++ frontend/pages/letter/preview.js
@@ -78,7 +78,6 @@ export default function Preview() {
         bgcolor: "#FCFAEF",
       }}
     >
-<<<<<<< HEAD
       <Header title="미리보기"></Header>
       <Box sx={{ display: "flex", justifyContent: "center" }}>
         {/* 포토카드 */}
@@ -89,10 +88,6 @@ export default function Preview() {
           content="test"
         ></Photocard>
       </Box>
-
-=======
-      {/* <Header title="미리보기"></Header> */}
->>>>>>> bb6164f703972862c2660d000e93edd6911077b3
       <audio
         src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
         ref={audioPlayer}
