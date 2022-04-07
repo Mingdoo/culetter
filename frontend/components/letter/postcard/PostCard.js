0860d988899f42163b48d3e53b7c2ee3f27866fd
Mandu <alonemint@ajou.ac.kr>
2022-04-07 20:54:38 +0900

    fix: Fix module import error

--- frontend/components/letter/postcard/PostCard.js
+++ frontend/components/letter/postcard/PostCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Box, Typography } from "@mui/material";
 import { colors } from "../../../components/Variables";
 import Palette from "../Palette";
